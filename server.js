c69fbda39949b471cf16a4f8b7c0f7ff1af89d26
Luke <68408655+lukeoxner@users.noreply.github.com>
2021-03-18 19:09:26 -0500

    add notes server.js

--- server.js
+++ server.js
@@ -7,22 +7,23 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// setting up all our middleware
 app.use(logger('dev'));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static('public'));
 
+// setting up our db connection to either use localhost or deployed location
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
 	useNewUrlParser: true,
 	useFindAndModify: false,
 });
 
-// routes
+// requiring in our routes file
 app.use(require('./routes/api.js'));
 
+// starting the server listening on the port we chose earlier in this file
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
 });
